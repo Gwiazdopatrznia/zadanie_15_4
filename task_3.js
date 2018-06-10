4336b6000b9bd0c4769f8630496e37c486ca1a32
Anna Gajewska <gajewska.anna@o2.pl>
2018-06-10 02:23:08 +0200

    Change in task_3.js

--- task_3.js
+++ task_3.js
@@ -1,5 +1,13 @@
-const average = (...theArgs) => console.log(theArgs.reduce((sum, next) => sum + next)/theArgs.length);
+const average = (...theArgs) => (theArgs.reduce((sum, next) => sum + next) / theArgs.length);
 
 average(1);
 average(1, 3);
 average(1, 3, 6, 6);
+
+let theArgs = [ 1 ];
+let theArgs2 = [ 1, 3 ];
+let theArgs3 = [ 1, 3, 6, 6 ];
+
+console.log(average(...theArgs));
+console.log(average(...theArgs2));
+console.log(average(...theArgs3));
