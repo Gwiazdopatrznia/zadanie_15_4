8f6dc3083aed59959f8314de30d01ae328fc557b
Anna Gajewska <gajewska.anna@o2.pl>
2018-06-19 23:14:32 +0200

    Change task_4

--- task_4.js
+++ task_4.js
@@ -1,3 +1,13 @@
-const averange = (...grades) => grades.reduce((sum, next) => sum + next)/grades.length;
-const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-console.log(averange(...grades));
+// const averange = (...grades) => grades.reduce((sum, next) => sum + next)/grades.length;
+// const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+// console.log(averange(...grades));
+
+console.log('task_4');
+ const a = [
+    { a: 1 },
+    { a: 5 },
+    { a: 24 },
+    { a: 0 }
+]
+const averange = (...a) => a.reduce((b, c) => b + c.a, 0);
+console.log(averange(...a));
